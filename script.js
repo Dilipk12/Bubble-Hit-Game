3d5aab61ac85d3a71d4c0c341ff4ffecd4fffdb4
Dilip Kumar <dilipkumar6200312@gmail.com>
2023-08-16 19:00:58 +0530

    Responsiveness added in the project

--- script.js
+++ script.js
@@ -7,23 +7,25 @@ let timersecond = 59;
 let scorenum = 0;
 let rn = 0;
 
+//  Code of Generate the Random Number inside the circle
+
 const circlerandomnum = () =>{
     let item = "";
-    for(let i=0; i<105; i++)
+    for(let i=0; i<50; i++)
     {
-    let randomnum = Math.floor(Math.random()*25);
+    let randomnum = Math.floor(Math.random()*10);
     item += `<button class="btn">${randomnum}</button>`
     }
     circle.innerHTML = item;
 }
 
+//  Code of timer part
 
 const timersec = () =>{
     var timerint = setInterval(() => {
         if(timersecond>0){
             timersecond--;
             timer.textContent = timersecond;
-            
         }
         else{
             clearInterval(timerint)
@@ -32,17 +34,21 @@ const timersec = () =>{
     }, 1000);
 };
 
+// Generate the Hit Random Number
 
 const hitrandomnum = () =>{
-    rn = Math.floor(Math.random()*25);
+    rn = Math.floor(Math.random()*10);
     hit.textContent = rn;
 }
+
+// Score Increase
 
 const increaseScore=()=>{
     scorenum = scorenum + 10;
     score.textContent = scorenum;
 }
 
+// code of Click event for Generate New Hit and Circle Number
 
 circle.addEventListener('click', (e)=>{
     let intnum = Number(e.target.textContent);
@@ -58,4 +64,3 @@ circle.addEventListener('click', (e)=>{
 timersec();
 circlerandomnum();
 hitrandomnum();
-
